9184836777bb50b9db66e897b6a4e1eedaaa7570
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2019-11-05 13:27:13 +0100

    Create Lista uppgift 1.cs

---
+++ Lista uppgift 1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+namespace lista1
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> tallista = new List<double>();
+                tallista.Add(1.5);
+                tallista.Add(2.5);
+                tallista.Add(3.5);
+                tallista.Add(4.5);
+            for (int i = 0; i < tallista.Count; i++)
+                Console.WriteLine(tallista[i]);
+
+                
+        }
+    }
+}
